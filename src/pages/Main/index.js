3984a681a0e251da14a0b694507454b9dcec0b9e
cyberCris <cristiano.adlim@gmail.com>
2019-12-24 20:45:46 -0300

    styled components

---
+++ src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+export default function main() {
+    return (
+        <Title error>
+            Main<small>menor</small>
+        </Title>
+    );
+}
